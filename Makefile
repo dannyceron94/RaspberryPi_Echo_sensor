d0ebe5f9c8d7aa5c78f23adaff330e5eaeca13d8
dannyceron94 <dannyceron94@gmail.com>
2020-03-07 20:31:28 -0800

    1st commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+assignment2: assignment2.c
+	gcc -I -Wall assignment2.c -lwiringPi -o assignment2
+rm:
+	rm assignment2
---
+++ assignment2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <time.h>
+
+#define ECHO 31
+#define TRIGGER 29
+
+void startUpEcho();
+double recordedTime(); 
+
+int main(void){
+
+    startUpEcho();
+    double distance = recordedTime()*(340/2);
+    return 0;
+}
+
+void startUpEcho(){
+
+    wiringPiSetup();
+    pinMode(TRIGGER,OUTPUT);
+    pinMode(ECHO,INPUT);
+    digitalWrite(TRIGGER,LOW);
+    delay(0.01);//.00001s
+    printf("setup function call\n");
+}
+
+double recordedTime(){
+    clock_t start, end;
+    printf("recorded function");
+    while(digitalRead(ECHO) == LOW){
+        printf("echo off\n");
+        start = clock();
+    }
+    while(digitalRead(ECHO)==HIGH){
+        end = clock();
+        printf("echo tue\n");
+    }
+
+    return ((double)(end-start))/CLOCKS_PER_SEC;
+}
